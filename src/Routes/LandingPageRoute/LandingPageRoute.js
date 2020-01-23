7e5d12b4f21920219805deb0daeb874ed7d165af
Erik Kjernlie <erik.kjernlie@gmail.com>
2020-01-23 16:55:54 +0100

    add confetti

--- src/Routes/LandingPageRoute/LandingPageRoute.js
+++ src/Routes/LandingPageRoute/LandingPageRoute.js
@@ -1,16 +1,39 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import "./index.css";
 import history from "src/history";
 import ButtonBroker from "src/components/ButtonBroker";
 import UserContext from "src/context/UserContext";
 import JoinPartyScreen from "src/components/JoinPartyScreen";
+import ConfettiCanvas from "react-confetti-canvas";
+import * as confetti from "canvas-confetti";
 
 export default function LandingPageRoute() {
   const { user, loading } = useContext(UserContext);
 
+  useEffect(() => {}, []);
+
   return (
     <div>
+      <div className="confetti">
+        <ConfettiCanvas />
+      </div>
       <div>Welcome to the party</div>
+      <div className="button">
+        <ButtonBroker
+          onClick={() => {
+            confetti.create()({
+              shapes: ["square"],
+              particleCount: 100,
+              spread: 90,
+              x: 1,
+              y: 1
+            });
+          }}
+        >
+          PARTY
+        </ButtonBroker>
+      </div>
+
       {loading && <div>Loading</div>}
       {!user && !loading && (
         <div className="flex">
